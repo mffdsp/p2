19cf1a121bc77e2bd774a03817e7394abf195a18
mffdsp <45128866+mffdsp@users.noreply.github.com>
2018-11-21 22:05:04 -0200

    /Exercícios para treinar Huffman

---
+++ Questoes_Bin_Tree.c
@@ -0,0 +1,15 @@
+https://practice.geeksforgeeks.org/problems/huffman-encoding/0
+https://www.thehuxley.com/problem/546
+https://www.thehuxley.com/problem/949
+https://www.thehuxley.com/problem/627
+https://www.thehuxley.com/problem/783
+
+
+
+
+
+
+
+
+
+** ====> Done;
