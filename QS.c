de0f620df4ed6c0945fe78653c2f83c268aa463c
mffdsp <45128866+mffdsp@users.noreply.github.com>
2018-12-05 16:03:20 -0300

    Update QS.c

--- QS.c
+++ QS.c
@@ -1,6 +1,6 @@
 
 //O código possuia erro nas linhas 18 e 19, pois os indices a e/ou b, poderiam assumir valores abaixo de 0 ou acima do tamanho do array;
-//
+//Tbm, para casos com numeros repetidos a correção está linha 24;
 
 
 
@@ -19,7 +19,11 @@ void quick_sort(int *v, int size){
 	    	while(v[a]<pivor && a<size) a++; // 18
 	    	while(v[b]>pivor && b>=0) b--;   // 19
 	    	if (a<=b)
+		{
 	    		swap(&v[a],&v[b]);
+			a++;b--;                 //24
+		}
+		    
 	    }
 	    quick_sort(v,b);
 	    quick_sort(v+a,size-a);
